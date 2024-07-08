ad61de63fd121fc7756495798b9528001f030272
behailu-2023 <142741099+behailu-2023@users.noreply.github.com>
2024-07-08 15:43:33 +0200

    Integrating an APM tool

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+import * as atatus from 'atatus-spa';
+atatus.config('0e603de348524e62ac1e4f811ec3d256').install();
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -21,3 +26,4 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
