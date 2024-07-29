74a365217ab347ce7bd90f96b26a4b593ad33e8a
behailu-2023 <142741099+behailu-2023@users.noreply.github.com>
2024-07-29 22:48:24 +0200

    Update

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -15,7 +15,7 @@ const EventGenresChart = ({ events }) => {
 
     useEffect(() => {
         setData(getData());
-    }, [`${events}`]);
+    }, [events]);
 
     const getData = () => {
         const data = genres.map(genre => {
